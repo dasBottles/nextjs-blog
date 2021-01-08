25245b6c38365f98b2a6b1cda908a43d0a411c2f
David Le <arattanasack@gmail.com>
2021-01-08 03:39:16 -0800

    move FA reference

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Link from 'next/link'
+
 
 const Navbar = () => {
   return (
@@ -6,17 +8,21 @@ const Navbar = () => {
       <nav className="navbar">
         <ul className="navbar-nav">
           <li className="logo">
-            <a href="#" className="nav-link">
-              <span className="link-text logo-text">andrew.exe</span>
-              <i className="fas fa-angle-double-right fa-primary" />
-            </a>
+            <Link href='/'>
+              <a className="nav-link">
+                <span className="link-text logo-text">andrew.exe</span>
+                <i className="fas fa-angle-double-right fa-primary" />
+              </a>
+            </Link>
           </li>
 
           <li className="nav-item">
-            <a href="#" className="nav-link">
-              <i className="fas fa-primary fa-flask fa-primary" />
-              <span className="link-text">Projects</span>
-            </a>
+            <Link href='/projects'>
+              <a href="#" className="nav-link">
+                <i className="fas fa-primary fa-flask fa-primary" />
+                <span className="link-text">Projects</span>
+              </a>
+            </Link>
           </li>
 
           <li className="nav-item">
--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,8 @@ class MyDocument extends Document {
                 <Head>
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&family=Xanh+Mono&display=swap" rel="stylesheet" />
+                <script src="https://kit.fontawesome.com/384bb1b9fd.js" crossOrigin="anonymous"></script>
+
                 </Head>
                 <body>
                     <Main/>
