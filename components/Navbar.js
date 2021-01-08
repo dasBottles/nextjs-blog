3140e3aa01e7bc793aa2bd438b7bb8f8959d8270
David Le <arattanasack@gmail.com>
2021-01-08 04:50:34 -0800

    add theme changer

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,37 @@
 import React from 'react'
 import Link from 'next/link'
 
+const darkBtn = document.getElementById('dark');
+const lightBtn = document.getElementById('light');
+const body = document.body;
+
+
+// Apply the cached theme on reload
+
+const theme = localStorage.getItem('theme');
+
+if (theme) {
+  body.classList.add(theme);
+}
+
+// Btn Event Handlers
+
+darkBtn.onclick = () => {
+  body.classList.replace('light', 'dark');
+  localStorage.setItem('theme', 'dark');
+};
+
+lightBtn.onclick = () => {
+  body.classList.replace('dark', 'light');
+
+  localStorage.setItem('theme', 'light');
+};
+
+
+
 
 const Navbar = () => {
+
   return (
     <>
       <nav className="navbar">
---
+++ pages/themeSwitch.js
@@ -0,0 +1,27 @@
+const darkBtn = document.getElementById('dark');
+const lightBtn = document.getElementById('light');
+const body = document.body;
+
+
+// Apply the cached theme on reload
+
+const theme = localStorage.getItem('theme');
+
+if (theme) {
+  body.classList.add(theme);
+}
+
+// Btn Event Handlers
+
+darkBtn.on = ('click', () => {
+    alert('test');
+//   body.classList.replace('light', 'dark');
+//   localStorage.setItem('theme', 'dark');
+});
+
+lightBtn.onclick = () => {
+  body.classList.replace('dark', 'light');
+
+  localStorage.setItem('theme', 'light');
+};
+
